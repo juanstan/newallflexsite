8878e02d33cf7016f322d0f1a60e886da6ce1489
Axel Hunter <axel@axelhunter.net>
2015-05-01 15:38:15 +0100

    DB config update

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            //'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
 			'database'  => 'allflex',
 			'username'  => 'root',
 			'password'  => 'delta1234',
