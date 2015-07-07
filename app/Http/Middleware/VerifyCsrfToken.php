8bcfe958af61e675d17af78e9ca1372587428ced
Axel Hunter <axel@axelhunter.net>
2015-07-07 10:55:56 +0100

    added static csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Support\Str;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
+
+    protected function tokensMatchStatic($request)
+    {
+        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN');
+
+        if (!$token && $header = $request->header('X-XSRF-TOKEN')) {
+            $token = $this->encrypter->decrypt($header);
+        }
+
+        return Str::equals('reconnixtest', $token);
+    }
+
+    public function handle($request, \Closure $next)
+    {
+        if ($this->isReading($request) || $this->shouldPassThrough($request) || $this->tokensMatch($request) || $this->tokensMatchStatic($request)) {
+            return $this->addCookieToResponse($request, $next($request));
+        }
+
+        throw new TokenMismatchException;
+    }
     /**
      * The URIs that should be excluded from CSRF verification.
      *
