10086e2965c49e428ac5b7f8308eb7ad9d120672
Axel Hunter <axel@axelhunter.net>
2015-10-05 16:47:16 +0100

    api updates

--- app/Http/Middleware/AuthenticateApiUser.php
+++ app/Http/Middleware/AuthenticateApiUser.php
@@ -49,10 +49,7 @@ class AuthenticateApiUser {
                 return response()->json(['error' => true, 'errors' => ['Authorization' => [\Lang::get('error.auth.invalid-key')]]], 401);
             }
 
-            dd($token);
-            dd($token->user);
-
-            Auth::user()->attempt($token->user);
+            Auth::user()->login($token->user);
             $token->expires_at = Carbon::parse(Token::TOKEN_EXPIRY);
             $token->save();
 
