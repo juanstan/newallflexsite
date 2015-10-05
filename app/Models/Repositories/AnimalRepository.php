3d225b73d36b800bf2bd3fca2594207dcbc72c49
Axel Hunter <axel@axelhunter.net>
2015-10-05 16:21:50 +0100

    restored animal repo

--- app/Models/Repositories/AnimalRepository.php
+++ app/Models/Repositories/AnimalRepository.php
@@ -8,10 +8,29 @@ class AnimalRepository extends AbstractRepository implements AnimalRepositoryInt
     protected $classname = 'App\Models\Entities\Animal';
     
     protected $repository;
-    
+
     public function __construct(UserRepositoryInterface $userRepositoryInterface)
     {
         $this->user = $userRepositoryInterface;
+    }
+
+    public function all()
+    {
+        if($this->user)
+        {
+            return $this->user->animals()->get();
+        }
+
+        return parent::all();
+    }
+
+    public function get($id)
+    {
+        if($id)
+        {
+            return $this->user ? $this->user->animals()->findOrFail($id) : parent::get($id);
+        }
+
     }
 
     public function getCreateValidator($input)
