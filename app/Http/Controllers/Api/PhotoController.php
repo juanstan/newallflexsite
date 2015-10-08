fd9688880259382b017bfcbd2ba294c636fbba0d
Axel Hunter <axel@axelhunter.net>
2015-10-08 11:13:33 +0100

    updated photocontroller

--- app/Http/Controllers/Api/PhotoController.php
+++ app/Http/Controllers/Api/PhotoController.php
@@ -39,9 +39,6 @@ class PhotoController extends Controller
             'location' => $this->photoRepository->uploadImage($request['image_path'], $user)
         );
 
-        dd($photo);
-
-
         $photo = $this->photoRepository->createForUser($photo, $user);
 
 
