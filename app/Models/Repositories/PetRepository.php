c593e5de9631040c77b25cfb43ed30e6e2cc26c7
j.acevedo@sureflap.co.uk <j.acevedo@sureflap.co.uk>
2015-12-11 15:19:31 +0000

    Commit related to the 843 issue

--- app/Models/Repositories/PetRepository.php
+++ app/Models/Repositories/PetRepository.php
@@ -38,7 +38,10 @@ class PetRepository extends AbstractRepository implements PetRepositoryInterface
     {
         return Validator::make($input,
         [
-
+            'name' 			=> ['sometimes','required'],
+            'date_of_birth' => ['sometimes','required','date'],
+            'weight'     	=> ['sometimes','required'],
+            'gender'		=> ['sometimes','required']
         ]);
     }
 
